d4a7f7a66674d91a870f4ebbb054f20c884690fb
TinaLoh <tinalola@gmail.com>
2015-02-10 14:31:13 -0800

    added navbar with home portfolio resume

---
+++ js/router.js
@@ -0,0 +1,11 @@
+App.Router.map(function() {
+  // put your routes here
+  this.resource('portfolio');
+  this.resource('resume');
+});
+
+// App.IndexRoute = Ember.Route.extend({
+//   model: function() {
+//     return ['red', 'yellow', 'blue'];
+//   }
+// });
